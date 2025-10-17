fc9e698f35d412d2f3c4b6dcc706e8297a90573d
MaiGamesDev <94523847+MaiGamesDev@users.noreply.github.com>
2025-10-17 22:26:50 +0900

    10/17 기초 인게임 UI

---
+++ Assets/01.Scripts/StyleManager.cs
@@ -0,0 +1,100 @@
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StyleManager : MonoBehaviour
+{
+    private Canvas targetCanvas;
+    [SerializeField] private GameObject textGrid;
+    [SerializeField] private TextMeshProUGUI combo;
+    [SerializeField] private Animation comboAnim;
+    [SerializeField] private Slider comboBar;
+
+
+    public TMP_FontAsset font;
+
+    public int comboes = 0;
+
+    private bool comboStopped = false;
+
+
+    void Start()
+    {
+        targetCanvas = FindFirstObjectByType<Canvas>();
+        RestoreComboBar();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CreateNewStyle();
+            SetCombo(comboes + 1);
+            RestoreComboBar();
+        }
+        DecreaseComboBar();
+    }
+    public TextMeshProUGUI CreateText(string name, string text, float size, Color color)
+    {
+        GameObject textObject = new GameObject(name);
+        textObject.transform.SetParent(textGrid.transform, true);
+        RectTransform rectTransform = textObject.AddComponent<RectTransform>();
+        TextMeshProUGUI tmpText = textObject.AddComponent<TextMeshProUGUI>();
+        DestroyTimer script = tmpText.AddComponent<DestroyTimer>();
+        rectTransform.sizeDelta = new Vector2(400, 100);
+
+        tmpText.text = text;                
+        tmpText.fontSize = size;           
+        tmpText.color = color;              
+        tmpText.alignment = TextAlignmentOptions.Left;
+        tmpText.font = font;
+
+        script.time = 3f;
+        
+
+        return tmpText;
+    }
+
+    public void CreateNewStyle()
+    {
+        float value = Mathf.Round(Random.Range(1f, 10f));
+        CreateText("style", " Àû Ã³Ä¡ +" + value + "%", 48, Color.white);
+        RemoveLatestStyle();
+
+    }
+
+    public void RemoveLatestStyle()
+    {
+        if (textGrid.transform.childCount > 7)
+        {
+            Destroy(textGrid.transform.GetChild(0).gameObject);
+        }
+    }
+
+    public void SetCombo(int value)
+    {
+        if (comboes < value)
+        {
+            comboAnim.Stop();
+            comboAnim.Play();
+        }
+        comboes = value;
+        combo.text = value + "x";
+    }
+    public void DecreaseComboBar()
+    {
+        comboBar.value -= 1 * Time.deltaTime;
+        if (comboBar.value <= 0 && !comboStopped)
+        {
+            SetCombo(0);
+            comboStopped = true;
+        }
+
+    }
+    public void RestoreComboBar()
+    {
+        comboStopped = false;
+        comboBar.value = comboBar.maxValue;
+    }
+}
--- Assets/01.Scripts/UIManager.cs
+++ Assets/01.Scripts/UIManager.cs
@@ -1,27 +1,79 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+
     public Slider hpBar;
+    public Slider ohBar;
+    public TextMeshProUGUI ohBarText;
+    public Slider stmBar;
 
     float hp = 100f;
     float maxHp = 100f;
+    float oh = 100f;
+    float maxOh = 100f;
+    float stm = 1;
+    float maxStm = 1;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        StatusReset();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            SetHp(hp + 1);
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            SetHp(hp - 1);
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SetOh(oh + 1);
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            SetOh(oh - 1);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            SetStm(0);
+        }
+        if (stm < 1) SetStm(stm + 0.5f * Time.deltaTime);
     }
-    void SetHpBar(float value)
+    void SetHp(float value)
     {
-        hp = value;
-        
+        hp = Mathf.Clamp(value, 0, maxHp);
+        if (hp > maxHp) hp = maxHp;
+        hpBar.maxValue = maxHp;
+        hpBar.value = hp;
+    }
+    void SetOh(float value)
+    {
+        oh = Mathf.Clamp(value, 0, maxOh);
+        ohBar.maxValue = maxOh;
+        ohBarText.text = oh + "%";
+        ohBar.value = oh;
+    }
+    void SetStm(float value)
+    {
+        stm = Mathf.Clamp(value, 0, maxStm);
+        if (stm > maxStm) hp = maxHp;
+        stmBar.maxValue = maxStm;
+        stmBar.value = stm;
+    }
+    
+    void StatusReset()
+    {
+        SetHp(maxHp);
+        SetOh(maxOh);
+        SetStm(maxStm);
     }
 }
