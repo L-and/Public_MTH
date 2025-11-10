940c965b666828a77cc4a5fa2f8ff20ae43305a8
Thongmin <wgshdm0627@gmail.com>
2025-11-10 16:57:33 +0900

    다시 수정
    11.10 (월)
    
    - 이전 수정사항 적용 안돼서 다시 수정 올림
    
    아래는 수정내역
    - MapManager.cs에 테스트용으로 딜레이 3초 했던것 주석처리
    - 보스방 Layer Ground 변경
    - Constants.cs 정리 (위치 변경)
    - MapManager.cs 테스트용으로 딜레이(3초) 제거, 일반 층, 보스 층 구별 스크립트 수정
    - PlayerSpawnManager.cs에서 플레이어 스폰 관련해서 PlayerManager로 호출 하도록 수정
    - MainMenuUIManager.cs가 실행되면 층이 초기화 되도록 스크립트 1줄 추가
    - 보스 방 문 이상하게 열리는 현상 수정
    - PlayerSpawn 관련 스크립트 정리 (PlayerSpawnManager.cs)

--- Assets/01.Scripts/Manager/MapManager.cs
+++ Assets/01.Scripts/Manager/MapManager.cs
@@ -114,10 +114,13 @@ public class MapManager : MonoBehaviour
     // 3) 현재 레벨(층)이 보스인지 일반레벨인지 체크
     switch (thisFloorConcept)
     {
-      case Constants.NORMAL_ROOM:
+      case Constants.MAP_PROTOTYPE:
+      case Constants.MAP_SEWER:
+      case Constants.MAP_UNDERGROUNDPRISON:
+      case Constants.MAP_ICECAVE:
         NormalMapSetting();
         break;
-      case Constants.BOSS_ROOM:
+      case Constants.MAP_BOSS:
         BossMapSetting();
         break;
     }
